011d28f3799be4749746ad3ffeb42aca0520d6bf
Chanchal Kumari <chanchal.kumari@keysight.com>
2025-11-24 19:27:45 +0530

    Fix syntax error

--- src/test/java/com/example/validator/LoginValidatorTest.java
+++ src/test/java/com/example/validator/LoginValidatorTest.java
@@ -6,21 +6,32 @@ import static org.junit.jupiter.api.Assertions.*;
 public class LoginValidatorTest {
 
     @Test
-    public void testValidLogin() {
+    public void testValidCredentials() {
         LoginValidator validator = new LoginValidator();
         assertTrue(validator.validate("admin", "secret"));
     }
 
     @Test
-    public void testInvalidLogin() {
+    public void testInvalidCredentials() {
         LoginValidator validator = new LoginValidator();
-        assertFalse(validator.validate("user", "wrong"));
+        assertFalse(validator.validate("user", "pass"));
     }
 
     @Test
-    public void testNullInputs() {
+    public void testNullUsername() {
         LoginValidator validator = new LoginValidator();
-        assertFalse(validator.validate(null, null));
+        assertFalse(validator.validate(null, "secret"));
+    }
+
+    @Test
+    public void testNullPassword() {
+        LoginValidator validator = new LoginValidator();
+        assertFalse(validator.validate("admin", null));
+    }
+
+    @Test
+    public void testEmptyCredentials() {
+        LoginValidator validator = new LoginValidator();
+        assertFalse(validator.validate("", ""));
     }
 }
-
